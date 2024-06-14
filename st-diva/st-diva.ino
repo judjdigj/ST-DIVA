e639fb209b29dd935c4a693d158acd9f8020aeb2
judjdigj <67326129+judjdigj@users.noreply.github.com>
2024-06-14 15:11:39 +0800

    Update st-diva.ino

--- st-diva/st-diva.ino
+++ st-diva/st-diva.ino
@@ -3,7 +3,8 @@
 #include "Adafruit_MPR121.h"
 
 // You can have up to 4 on one i2c bus but one is enough for testing!
-Adafruit_MPR121 cap = Adafruit_MPR121();
+Adafruit_MPR121 cap1 = Adafruit_MPR121();
+Adafruit_MPR121 cap2 = Adafruit_MPR121();
 
 // Keeps track of the last pins touched
 // so we know when buttons are 'released'
@@ -21,52 +22,59 @@ void setup() {
   while (!Serial) { // needed to keep leonardo/micro from starting too fast!
     delay(10);
   }
-  cap.begin(0x5A);
-//  cap2.begin(0x5B);
+//  cap1.begin(0x5A);
+//  cap2.begin(0x5C);
 //  cap3.begin(0x5C);
 
-
-  /*
-  if (!cap.begin(0x5A)) {
-    Serial.println("MPR121 not found, check wiring?");
+  if (!cap1.begin(0x5A)) {
+    Serial.println("MPR121 0x5A not found, check wiring?");
     while (1);
   }
-  Serial.println("MPR121 found!");
-*/
+  Serial.println("MPR121 0x5A found!");
+
+  if (!cap2.begin(0x5B)) {
+    Serial.println("MPR121 0x5B not found, check wiring?");
+    while (1);
+  }
+  Serial.println("MPR121 0x5B found!");
+
 }
 void loop() {
+  if(cap1.touched() != 0 && cap2.touched() == 0){
+    lasttouched = currtouched;
+    currtouched = log(cap1.touched())/log(2) + 1;
+  }
+  if(cap1.touched() == 0 && cap2.touched() != 0){
+    lasttouched = currtouched;
+    currtouched = log(cap2.touched())/log(2) + 13;
+  }
 
-  lasttouched = currtouched;
-  currtouched = log(cap.touched())/log(2) + 1;
 
   //Direction detection
   if(abs(lasttouched - currtouched) == 1){
     if(lasttouched - currtouched > 0){
       direction = 0;
-      touch_status = 1;
     }
     else if(lasttouched - currtouched < 0){
       direction = 1;
-      touch_status = 1;
     }
+    touch_status = 1;
   }
 
   //Key pressing process
   if(touch_status == 1){
-    if (cap.touched() == 0 ){
+    if(cap1.touched() == 0 && cap2.touched() == 0){
       Keyboard.release('q');
       Keyboard.release('e');
       touch_status = 0;
     }
-    else{
-      if(direction == 0){
-        Keyboard.release('e');
-        Keyboard.press('q');
-      }
-      else{
-        Keyboard.release('q');
-        Keyboard.press('e');
-      }
+    else if(direction == 0){
+      Keyboard.release('e');
+      Keyboard.press('q');
+    }
+    else if(direction == 1){
+      Keyboard.release('q');
+      Keyboard.press('e');
     }
   }
 }
