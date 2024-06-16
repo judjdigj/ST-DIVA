1efa92b9520469bdef9f3aa892701feb8cea3244
judjdigj <67326129+judjdigj@users.noreply.github.com>
2024-06-16 20:58:31 +0800

    Update st-diva.ino

--- st-diva/st-diva.ino
+++ st-diva/st-diva.ino
@@ -5,6 +5,7 @@
 // You can have up to 4 on one i2c bus but one is enough for testing!
 Adafruit_MPR121 cap1 = Adafruit_MPR121();
 Adafruit_MPR121 cap2 = Adafruit_MPR121();
+Adafruit_MPR121 cap3 = Adafruit_MPR121();
 
 // Keeps track of the last pins touched
 // so we know when buttons are 'released'
@@ -14,8 +15,6 @@ int currtouched = 0;
 bool touch_status = 0;
 bool direction = 0; //0 for left, 1 for right
 bool multitouch = 0;
-
-const int touchKey[4] = {"Q","E","1","3"};
 
 void setup() {
   Serial.begin(9600);
@@ -33,7 +32,13 @@ void setup() {
   }
   Serial.println("MPR121 0x5A found!");
 
-  if (!cap2.begin(0x5C)) {
+  if (!cap2.begin(0x5B)) {
+    Serial.println("MPR121 0x5C not found, check wiring?");
+    while (1);
+  }
+  Serial.println("MPR121 0x5C found!");
+
+  if (!cap3.begin(0x5C)) {
     Serial.println("MPR121 0x5C not found, check wiring?");
     while (1);
   }
@@ -41,17 +46,8 @@ void setup() {
 
 }
 void loop() {
-
-  if(cap1.touched() != 0 && cap2.touched() == 0){
-    lasttouched = currtouched;
-    currtouched = log(cap1.touched())/log(2) + 1;
-  }
-  if(cap1.touched() == 0 && cap2.touched() != 0){
-    lasttouched = currtouched;
-    currtouched = log(cap2.touched())/log(2) + 13;
-  }
-
-//  Serial.println(currtouched);
+  lasttouched = currtouched;
+  currtouched = Formatted();
 
   //Touch status and direction detection
   if(abs(lasttouched - currtouched) == 1){
@@ -64,37 +60,36 @@ void loop() {
     touch_status = 1;
   }
 
-  //Direction detection
-  if(abs(lasttouched - currtouched) == 1){
-    if(lasttouched - currtouched > 0){
-      direction = 0;
-      touch_status = 1;
-    }
-    else if(lasttouched - currtouched < 0){
-      direction = 1;
-      touch_status = 1;
-    }
-  }
 //  if(abs(lasttouched - currtouched > 6)){
 //    multitouch = 1;
 //  }
 
   //Key pressing process
   if(multitouch == 0 && touch_status == 1){
-    if (cap1.touched() == 0 && cap2.touched() == 0){
-      Keyboard.release('q');
+    if(direction == 0){
       Keyboard.release('e');
-      touch_status = 0;
+      Keyboard.press('q');
     }
     else{
-      if(direction == 0){
-        Keyboard.release('e');
-        Keyboard.press('q');
-      }
-      else{
-        Keyboard.release('q');
-        Keyboard.press('e');
-      }
+      Keyboard.release('q');
+      Keyboard.press('e');
     }
+    touch_status = 0;
   }
+}
+
+int Formatted(){
+  if(cap1.touched() != 0 && cap2.touched() == 0 && cap3.touched() == 0){
+    return currtouched = log(cap1.touched())/log(2) + 1;
+  }
+  if(cap1.touched() == 0 && cap2.touched() != 0 && cap3.touched() == 0){
+    return currtouched = log(cap2.touched())/log(2) + 13;
+  }
+  if(cap1.touched() == 0 && cap2.touched() == 0 && cap3.touched() != 0){
+    return currtouched = log(cap3.touched())/log(2) + 25;
+  }
+  if(cap1.touched() == 0 && cap2.touched() == 0 && cap3.touched() == 0){
+    return 0;
+  }
+
 }
