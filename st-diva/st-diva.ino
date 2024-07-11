edf50f1474bd349ebb29c5e9d648100ce032b309
judjdigj <67326129+judjdigj@users.noreply.github.com>
2024-07-11 21:10:20 +0800

    Update st-diva.ino

--- st-diva/st-diva.ino
+++ st-diva/st-diva.ino
@@ -28,18 +28,29 @@ int touch_status[4] = {0};
 bool iftouched = 0;
 uint16_t thresholds = 0;
 
+uint16_t lasttouched1 = 0;
+uint16_t currtouched1 = 0;
+uint16_t lasttouched2 = 0;
+uint16_t currtouched2 = 0;
+uint16_t lasttouched3 = 0;
+uint16_t currtouched3 = 0;
+
+int curr = 0;
+int last = 0;
+
 void setup() {
   Serial.begin(9600);
   cap1.begin(0x5A);
   cap2.begin(0x5B);
   cap3.begin(0x5C);
   Keyboard.begin();
-  touchCalibrate();
+  delay(1000);
+///  touchCalibrate();
 }
 
 
 void loop() {
-  touchpadReaderRAW();
+  touchpadReader();
 
   #ifdef DEBUG
   for(uint16_t i=0; i<36; i++){
@@ -50,52 +61,27 @@ void loop() {
 
   #ifndef DEBUG
   //Last = Curr
-  for(uint16_t i=0; i<2; i++){
-    last_1[i] = curr_1[i];
-    last_2[i] = curr_2[i];
-    curr_1[i] = 0;
-    curr_2[i] = 0;
-  }
+  last = curr;
+  curr = 0;
 
   //Itering touch status
-  uint16_t j=0;
-  iftouched = 0;
   for(uint16_t i=0; i<36; i++){
     if(touchpad[i]){
-      if(j<4){
-        touch_status[j] = i;  //4 point maximium
-        j++;
-        iftouched = 1;
-      }
+      curr = i;  //4 point maximium
+      iftouched = 1;
     }
   }
-
-  if(!iftouched){
-    for(uint16_t i=0; i<4; i++){
-      touch_status[i] = 0;
-    }
+ //direction detection
+  if(iftouched){
+    slideDetect(curr, last);
   }
 
-  //Storage touched sensor into batch
-  for(uint16_t i=0; i<2; i++){
-    curr_1[i] = touch_status[i];
-    curr_2[i] = touch_status[i+2];
-  }
   //Slide status
-
-
-  slideDetect(curr_1[0], last_1[0], curr_1[1], last_1[1]);
 
   #endif
 }
 
 void touchpadReader(){
-  uint16_t lasttouched1 = 0;
-  uint16_t currtouched1 = 0;
-  uint16_t lasttouched2 = 0;
-  uint16_t currtouched2 = 0;
-  uint16_t lasttouched3 = 0;
-  uint16_t currtouched3 = 0;
 
 
   lasttouched1 = currtouched1;
@@ -165,17 +151,18 @@ void touchpadReaderRAW(){
   }
 }
 
-void slideDetect(int x, int y, int x1, int y1){
-  if(x + y + x1 + y1 == 0){
+void slideDetect(int x, int y){
+  Serial.print("Direction: ");
+  if(x + y == 0){
     Keyboard.releaseAll();
   }
-  else if(x - y == 1 && x1 - y1 == 1){
-    Keyboard.release('q');
+  else if(x - y == 1){
     Keyboard.press('e');
+    Keyboard.release('q');
   }
-  else if(x - y == -1 && x1 - y1 == -1){
-    Keyboard.release('e');
+  else if(x - y == -1){
     Keyboard.press('q');
+    Keyboard.release('e');
   }
 }
 
